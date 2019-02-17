4bea4c6c9eda8b43a2c8c673d3cb4c0ed4b3e62d
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-18 00:10:42 +0100

    Add implemented test for fibonacci

--- src/test/scala/com/emaginalabs/wecodeproperties/FibonacciSpec.scala
+++ src/test/scala/com/emaginalabs/wecodeproperties/FibonacciSpec.scala
@@ -2,10 +2,13 @@ package com.emaginalabs.wecodeproperties
 
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
+import org.scalacheck.Gen
+
 
 class FibonacciSpec extends FlatSpec with PropertyChecks with Matchers {
-
-  "Fibonacci of n" should "keep the most important property" in {
-    //please, find the property and test it. The implementation of fibonacci is slow so you can check what happen with big numbers ;)
+  "Fibonacci of n" should "be the sum of fibonacci of n-2 + fibonacci of n-1" in {
+    forAll(Gen.chooseNum(2, 1000)) { (n: Int) =>
+      Fibonacci(n) shouldBe Fibonacci(n - 2) + Fibonacci(n - 1)
+    }
   }
 }
