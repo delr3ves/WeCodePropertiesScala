b0f24ec86ed7be1d702903ac764c117705206e97
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-17 22:15:51 +0100

    Solve the exercises test to the library

--- src/test/scala/com/emaginalabs/wecodeproperties/WeCodePlaygroundSpec.scala
+++ src/test/scala/com/emaginalabs/wecodeproperties/WeCodePlaygroundSpec.scala
@@ -1,5 +1,6 @@
 package com.emaginalabs.wecodeproperties
 
+import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -45,20 +46,27 @@ class WeCodePlaygroundSpec extends FlatSpec with PropertyChecks with Matchers {
 
   "Playing with the library" should "add logs to know how it executes" in {
     forAll() { (a: Int, b: Int) =>
-      //write test the whatever property about the sum but add logs to know the value of the received numbers
+      println(s"Just executed the test with values [a: $a, b: $b]")
+      playground.sum(a, b) shouldBe playground.sum(b, a)
     }
   }
 
   it should "allow us to modify the number of executions" in {
-    //write a test in order to ensure the the number of executions
-    // Here, you'll find how to change the number of success executions: https://stackoverflow.com/a/38706052
+    var numOfExecutions = 0;
+    forAll(minSuccessful(500)) { (a: Int) =>
+      numOfExecutions += 1
+      succeed
+    }
+    numOfExecutions shouldBe 500
   }
 
   it should "make a test fail in order to see how shrink works" in {
     val result = Try {
       forAll() { (a: Int, b: Int) =>
-        //write a test that sometimes fail and add traces in order to know how the library tries to give you a clue about the failing data
-        fail("Just fail")
+        println(s"Just executed the test with values [a: $a, b: $b]")
+        if (a > 5 || b > 7) {
+          fail("Just fail")
+        }
       }
     }
     result match {
@@ -68,8 +76,15 @@ class WeCodePlaygroundSpec extends FlatSpec with PropertyChecks with Matchers {
   }
 
   it should "fail when a test is too restrictive" in {
-    //write a custom generator that is too restrictive to find enough values to run the tests.
-    //Tip: You can capture the error the same way the previous test :)
+    val result = Try {
+      forAll(Gen.choose(Int.MinValue, Int.MaxValue).filter(a => { a > 1 && a < 5 })) { (a: Int) =>
+        succeed
+      }
+    }
+    result match {
+      case Failure(exception) => print(exception.getMessage)
+      case Success(_) => fail("The property should fail because generator is to restritive")
+    }
   }
 
 }
