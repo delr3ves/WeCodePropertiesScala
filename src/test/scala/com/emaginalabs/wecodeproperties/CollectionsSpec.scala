467233fb40c6ebc220bdece781e6505514d3fac9
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-18 00:18:12 +0100

    Define properties for collections

---
+++ src/test/scala/com/emaginalabs/wecodeproperties/CollectionsSpec.scala
@@ -0,0 +1,26 @@
+package com.emaginalabs.wecodeproperties
+
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.prop.PropertyChecks
+
+class CollectionsSpec extends FlatSpec with PropertyChecks with Matchers {
+
+  "Collections" should "contain one more element after insertion" in {
+  }
+
+  it should "contain the inserted element" in {
+  }
+
+  it should "have an element less after deletion" in {
+  }
+
+  it should "not contain the removed element" in {
+  }
+
+  it should "contain the items of the two collections after a concatenation" in {
+  }
+
+  it should "no duplicated elements when convert to a set" in {
+  }
+
+}
