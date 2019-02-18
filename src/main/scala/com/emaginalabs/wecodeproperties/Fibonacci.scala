2d539423b050a8b53abecfc807797a428e60d7c2
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-18 09:17:37 +0100

    Fix styles

--- src/main/scala/com/emaginalabs/wecodeproperties/Fibonacci.scala
+++ src/main/scala/com/emaginalabs/wecodeproperties/Fibonacci.scala
@@ -6,11 +6,11 @@ object Fibonacci {
 
   def apply(n: Int): Long = {
     @tailrec
-    def fib_tail( n: Int, a:Long , b:Long): Long = n match {
+    def fib_tail(n: Int, a: Long, b: Long): Long = n match {
       case 0 => a
-      case _ => fib_tail( n-1, b, a+b )
+      case _ => fib_tail(n - 1, b, a + b)
     }
-    return fib_tail( n, 0, 1)
+    return fib_tail(n, 0, 1)
   }
 
 }
