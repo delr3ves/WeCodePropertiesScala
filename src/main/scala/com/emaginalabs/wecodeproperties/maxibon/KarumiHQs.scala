eef7c7949f4038470cc44cb22b977fa69d1829d6
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-21 18:44:09 +0100

    Add implementation

--- src/main/scala/com/emaginalabs/wecodeproperties/maxibon/KarumiHQs.scala
+++ src/main/scala/com/emaginalabs/wecodeproperties/maxibon/KarumiHQs.scala
@@ -3,9 +3,35 @@ package com.emaginalabs.wecodeproperties.maxibon
 import com.emaginalabs.wecodeproperties.maxibon.developer.Developer
 
 case class KarumiHQs(val chat: Chat) {
+  private val minMaxibons = 2
+  private val maxMaxibons = 10
 
-  def openFridge(developer: Developer) = ???
+  var maxibonsLeft: Int = maxMaxibons
 
-  def openFridge(developers: List[Developer]) = ???
+  def openFridge(developer: Developer) {
+    grabMaxibons(developer)
+    if (shouldBuyMoreMaxibons()) {
+      notifyWeShouldBuyMaxibon(developer)
+      buyMaxibons()
+    }
+  }
 
+  def openFridge(developers: List[Developer]) {
+    developers.foreach { developer =>
+      openFridge(developer)
+    }
+  }
+
+  private def grabMaxibons(developer: Developer) {
+    maxibonsLeft -= developer.numberOfMaxibonsToGrab
+  }
+
+  private def shouldBuyMoreMaxibons(): Boolean = maxibonsLeft <= minMaxibons
+
+  private def notifyWeShouldBuyMaxibon(developer: Developer) =
+    chat.sendMessage(s"Hi guys, I'm ${developer.name}. We need more maxibons!")
+
+  private def buyMaxibons() {
+    maxibonsLeft += maxMaxibons
+  }
 }
